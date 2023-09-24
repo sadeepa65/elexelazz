bbeadf51c5b7ec17fa02443691257e0e1f3fdd47
Sadeepa <srisadeepa658@gmail.com>
2023-09-24 16:45:02 +0530

    updaed

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function window(){
 window.location("services.html");
 
 }
+function facebook(){
+    window.location("https://wa.me/message/HPGMJSWXMN56B1");
+}
+function instagram(){
+    window.location("https://wa.me/message/HPGMJSWXMN56B1");
+}
+function whatsapp(){
+    window.location("https://wa.me/message/HPGMJSWXMN56B1");
+}
